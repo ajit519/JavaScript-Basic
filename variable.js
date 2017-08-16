63613228cc020c51c56c971571f86bb06cfa75a2
ajit519 <ajitcse519@gmail.com>
2017-08-16 21:45:49 +0530

    javascript variable sample

---
+++ variable.js
@@ -0,0 +1,32 @@
+/**
+ * JavaScript has three types of variables
+ * 1. Boolean
+ * 2. Numbers
+ * 3. String
+ * String is 16 bit unicode sequence character
+ * it doesn't have char data type
+ */
+
+var a = 10;
+var b;
+b = 20;
+
+console.log(a);
+console.log(b);
+console.log(a+b);
+
+var httpsEnabled= true;
+
+console.log(httpsEnabled);
+
+var firstName = "Ajit";
+
+var lastName = "Kumar";
+
+console.log(firstName + lastName); 
+
+// javascript doesn't support strong typing
+
+
+a = "Hello JavaScript !"
+console.log(a);
