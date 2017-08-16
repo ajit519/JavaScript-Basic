56fcecd733e59e3589c057dbf0f3fcd19363cf7e
ajit519 <ajitcse519@gmail.com>
2017-08-16 22:13:13 +0530

    undefined in javascript

---
+++ undefined.js
@@ -0,0 +1,17 @@
+/**
+ * This code sample demonstrate the
+ * understanding of undefined.
+ * To understand the undefined in
+ * javascript. we need to look
+ * definition and declaration
+ */
+
+ var firstName;   //variable declartion
+ 
+ console.log("value for variable firstName doesn't got assign so, javascript will give a default value ::" + firstName); 
+
+ firstName = "Sam"; // variable defination
+
+ console.log(firstName);
+
+  
