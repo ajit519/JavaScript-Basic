63e384514016e74b0fa1d0a3b6d91b249e9cf5d9
ajit519 <ajitcse519@gmail.com>
2017-08-19 10:01:40 +0530

    wrapper object

---
+++ wrapper.js
@@ -0,0 +1,15 @@
+/**
+  * Javascript wrapper object.
+  * similar to java, javascript has also wrapper
+  * object. for primitive data type Stirng boolean
+  * and number.
+  */
+
+var greeting = "Hello Javascript";
+
+console.log(greeting.length);
+
+// if we see the above print statement there is method assoicated 
+// with greeting object. so what javascript do it will create a 
+// wrapper object for string.
+  
