469fb3741686a8f9ecc1d6fc778373171d7660e4
ajit519 <ajitcse519@gmail.com>
2024-01-05 20:39:47 +0530

    save fix

--- delete.js
+++ delete.js
@@ -5,15 +5,7 @@
   var employee = {
    "firstName" : "Ajit",
 	 "lastName" : "Kumar",
-<<<<<<< HEAD
-<<<<<<< HEAD
-	 "middleName": "xyz"
-=======
 	 "middleName": "abc"
->>>>>>> added middle name as abc
-=======
-	 "middleName": "abc"
->>>>>>> 64a2735ba7c68117e87db65f45ebef238d17df70
   };
   
   delete employee.middleName;
